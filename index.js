07515b41f4ce502cc049c53a6510fcffddd39c34
Toshi <waykiyoharu@gmail.com>
2020-10-04 15:02:22 +0900

    add new file

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+// const port = 3000
+var port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Hello22 World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
